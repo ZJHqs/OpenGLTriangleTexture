0ba47fe061c0508e63446e439327ccb41b40edc1
ZhongJunhao <tim.zhong@lednets.com>
2023-04-10 20:51:25 +0800

    使笑脸上下颠倒
    上下颠倒可以参考下面文章：
    https://blog.csdn.net/narutojzm1/article/details/51940817
    
    Signed-off-by: ZhongJunhao <tim.zhong@lednets.com>

--- app/src/main/res/raw/triangle_fragment_shader.glsl
+++ app/src/main/res/raw/triangle_fragment_shader.glsl
@@ -8,6 +8,7 @@ varying vec2 v_TexCoord1;
 varying vec2 v_TexCoord2;
 
 void main() {
-    gl_FragColor = mix(texture2D(u_Texture1, v_TexCoord1), texture2D(u_Texture2, v_TexCoord2), 0.2)
+    gl_FragColor = mix(texture2D(u_Texture1, v_TexCoord1),
+    texture2D(u_Texture2, vec2(v_TexCoord2.x, 1.0 - v_TexCoord2.y)), 0.2)
                             * vec4(v_Color, 1.0);
 }
