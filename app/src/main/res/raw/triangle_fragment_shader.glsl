4e36586371505099f8cd88d154fd548e19479ef4
ZhongJunhao <tim.zhong@lednets.com>
2023-04-10 20:55:07 +0800

    使笑脸朝另一个方向

--- app/src/main/res/raw/triangle_fragment_shader.glsl
+++ app/src/main/res/raw/triangle_fragment_shader.glsl
@@ -9,6 +9,6 @@ varying vec2 v_TexCoord2;
 
 void main() {
     gl_FragColor = mix(texture2D(u_Texture1, v_TexCoord1),
-    texture2D(u_Texture2, vec2(v_TexCoord2.x, 1.0 - v_TexCoord2.y)), 0.2)
+    texture2D(u_Texture2, vec2(1.0 - v_TexCoord2.x, 1.0 - v_TexCoord2.y)), 0.2)
                             * vec4(v_Color, 1.0);
 }
